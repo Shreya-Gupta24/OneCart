9000a2b1cbf520d1fef7bc8e9b3d763dd5b619d1
Shreya-Gupta24 <shreyagupta.gupta244@gmail.com>
2026-02-21 04:05:51 +0530

    Update CORS origin to include production URL

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://one-cart-ecommerce.vercel.app", "http://localhost:5174"],
     credentials: true
 }))
 app.use((req, res, next) => {
