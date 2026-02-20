bad475a053dd31ce5abed75fb6278abea32b1045
Shreya-Gupta24 <shreyagupta.gupta244@gmail.com>
2026-02-21 03:59:56 +0530

    Fix import case for AuthContext in UserContext

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import { useContext } from 'react';
-import {authDataContext} from './authContext';
+import {authDataContext} from './AuthContext';
 import { useEffect } from 'react';
 import axios from 'axios';
 
